1917154771d6c0238a0edfc661880dd7b0a0db06
Marina Solovieva <marishasolovieva@mail.ru>
2020-05-30 20:37:02 +0300

    init commit

---
+++ src/components/Posts.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Post from "./Post";
+import {connect} from "react-redux";
+
+const Posts = ({syncPosts}) => {
+    if (!syncPosts.length) {
+        return <p className="text-center">Постов пока нет</p>
+    }
+    return syncPosts.map(post => <Post post={post} key={post.id}/>)
+};
+
+const mapStatetoProps = state => {
+    return {
+        syncPosts: state.posts.posts
+    }
+};
+
+export default connect(mapStatetoProps, null)(Posts)
--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { compose, createStore, applyMiddleware} from "redux";
+import {rootReducer} from "./redux/rootReducer";
+import {Provider} from "react-redux";
+import thunk from "redux-thunk"
+import {forbiddenWordsMiddleware} from "./redux/middleware";
+
+
+
+const store = createStore(rootReducer, compose(
+    applyMiddleware(thunk, forbiddenWordsMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+
+const app = (
+    <Provider store={store}>
+        <App/>
+    </Provider>
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    app, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
