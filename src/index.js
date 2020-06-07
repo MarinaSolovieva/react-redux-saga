bfc7ae9c2bc28ca22c1bc92fa6d002d2316db8b0
Marina Solovieva <marishasolovieva@mail.ru>
2020-06-07 22:29:13 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { compose, createStore, applyMiddleware} from "redux";
+import {compose, createStore, applyMiddleware} from "redux";
 import {rootReducer} from "./redux/rootReducer";
 import {Provider} from "react-redux";
 import thunk from "redux-thunk"
+import createSagaMiddleware from 'redux-saga'
 import {forbiddenWordsMiddleware} from "./redux/middleware";
+import {sagaWatcher} from "./redux/sagas";
 
 
+
+const saga = createSagaMiddleware();
 
 const store = createStore(rootReducer, compose(
-    applyMiddleware(thunk, forbiddenWordsMiddleware),
+    applyMiddleware(thunk, forbiddenWordsMiddleware, saga),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
+
+saga.run(sagaWatcher);
 
 
 const app = (
